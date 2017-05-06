d15a73010f615b204aa5928e011e73fef3d20cad
adrianZahra <adrian.zahra@my.jcu.edu.au>
2017-05-06 15:06:54 +1000

    2nd Commit: added a settings activity and and exit button code

--- app/src/main/java/com/example/example/educationapp/SettingsActivity.java
+++ app/src/main/java/com/example/example/educationapp/SettingsActivity.java
@@ -2,50 +2,37 @@ package com.example.example.educationapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    void startGame(View view){
-        //method launches the game when the start button is pressed
 
     }
-
-    void quitGame(View view){
-        //method closes app and saves data when the quit button is pressed
-        finishAffinity();
-        System.exit(0);
-
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Intent settingViewIntent = new Intent(this, SettingsActivity.class);
-            startActivity(settingViewIntent);
+        if (id == R.id.action_home) {
+            Intent homeViewIntent = new Intent(this, MainActivity.class);
+            startActivity(homeViewIntent);
             return true;
         }else if (id == R.id.action_highScore){
             //Intent highScoreViewIntent = new Intent(this, SettingsActivity.class);
@@ -54,4 +41,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
