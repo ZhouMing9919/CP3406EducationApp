a902e36c7498848803368a2057fbd6b8a3ed240f
adrianZahra <adrian.zahra@my.jcu.edu.au>
2017-05-06 10:34:11 +1000

    Initial commit

---
+++ app/src/main/java/com/example/example/educationapp/GameData.java
@@ -0,0 +1,5 @@
+package com.example.example.educationapp;
+
+//This is where the data needed to run the game will be created and stored
+public class GameData {
+}
---
+++ app/src/main/java/com/example/example/educationapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.example.educationapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    void startGame(View view){
+        //method launches the game when the start button is pressed
+
+    }
+
+    void quitGame(View view){
+        //method closes app and saves data when the quit button is pressed
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
