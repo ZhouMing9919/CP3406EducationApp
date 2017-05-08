62591b091f262c54964540aeb98aa9014f983d66
adrianZahra <adrian.zahra@my.jcu.edu.au>
2017-05-08 10:54:20 +1000

    4th Commit: added a countdown timer to handle the time of the game

--- app/src/main/java/com/example/example/educationapp/GameActivity.java
+++ app/src/main/java/com/example/example/educationapp/GameActivity.java
@@ -2,6 +2,7 @@ package com.example.example.educationapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+    private TextView timeField;
+    long clockTime = 10000;
+    public CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,23 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        timeField = (TextView) findViewById(R.id.timeField);
+        createTimer();
     }
+
+    public void createTimer() {
+        countDownTimer = new CountDownTimer(clockTime, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timeField.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                timeField.setText("done!");
+            }
+        }.start();
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
